e608a1e3132b58837d7d0bc83704ce10cc49aabc
Nathan McCarty <nathan@mccarty.io>
2015-11-29 14:53:49 -0500

    Add ability to train model Still can't produce using this though, so you might as well not

--- lib/markov_model.rb
+++ lib/markov_model.rb
@@ -1,3 +1,35 @@
 
 class MarkovModel
+  def initialize(n=1)
+    @model = Hash.new(Hash.new(0))
+    @order = n
+  end
+
+  # Accessor for model
+  # Uses the marshall hack to make a deep clone
+  def model
+    Marshal.load(Marshal.dump(@model))
+  end
+
+
+  def ngrams(list)
+    length = @order + 1
+    list = Array.new(@order, :start) + list + Array.new(@order, :end)
+    ngrams = []
+    (0..(list.length - length)).each do |i|
+      ngrams << list[i...i+length]
+    end
+    ngrams.freeze
+  end
+
+  def train(tokens)
+    ngrams = ngrams tokens
+    ngrams.each do |x|
+      state = x[0...-1]
+      transition = x[-1]
+      @model[state] = Hash.new(0) unless @model.key? state
+      @model[state][transition] += 1
+    end
+    nil
+  end
 end
